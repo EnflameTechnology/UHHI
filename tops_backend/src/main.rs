69a5db3f58995d3557ef274d54ee151022e40ab0
guoqingbao <topon@outlook.com>
2022-09-19 16:50:56 +0800

    Support matmul & transpose for tops_backend

--- tops_backend/src/main.rs
+++ tops_backend/src/main.rs
@@ -1,7 +1,7 @@
 use tops_backend as tops;
 use uhal::error::DeviceError;
 use uhal::{DriverLibraryTrait};
-use uhal::memory::DeviceBufferTrait;
+use uhal::memory::{DeviceBufferTrait, DevicePointerTrait};
 use uhal::stream::{StreamTrait, StreamFlags};
 use uhal::module::{ModuleTrait};
 use tops::module::TopsModule;
@@ -12,12 +12,13 @@ use tops::memory::CopyDestination;
 use uhal::launch;
 use tops_raw as driv;
 use std::ptr;
-use std::os::raw::c_void;
+use std::os::raw::{c_void, c_int};
 use std::ffi::CString;
 use driv::{topsError_t};
 use std::path::Path;
 use uhal::error::{DeviceResult};
-
+use cust_core::DeviceCopy;
+use std::collections::HashMap;
 
 #[cfg(unix)]
 fn to_bytes<P: AsRef<Path>>(path: P) -> Vec<u8> {
@@ -26,7 +27,13 @@ fn to_bytes<P: AsRef<Path>>(path: P) -> Vec<u8> {
 }
 
 fn legacy() -> DeviceResult<()>{
-    let ptx = "./resources/copy_d2d.o".to_string();
+
+    unsafe {
+        let mut device : driv::topsDevice_t = 0;
+        driv::topsDeviceGet(&mut device, 0);
+        driv::topsSetDevice(device as c_int);
+    }
+    let ptx = "./resources/copy_d2c.o".to_string();
 
     let mut bytes = to_bytes(ptx);
     if !bytes.contains(&0) {
@@ -40,7 +47,7 @@ fn legacy() -> DeviceResult<()>{
         );
     }
 
-    let name = "copy_d2d";
+    let name = "copy_d2c";
     let cstr = CString::new(name).expect("Argument to get_function had a nul");
     let mut func: driv::topsFunction_t = ptr::null_mut();
 
@@ -69,8 +76,8 @@ fn legacy() -> DeviceResult<()>{
         N: usize
     }
     
-    const N:usize = 100000;
-    let mut val = [0.5f32; N];
+    const N:usize = 10000;
+    let mut val = vec![0.5f32; N];
     const Nbytes:usize = N * 4;
 
     let mut host_ptr = std::ptr::null_mut();
@@ -141,8 +148,8 @@ fn legacy() -> DeviceResult<()>{
     }
 
 
-    let mut out_host = [0.0f32; N];
-    let mut out_host2 = [0.0f32; N];
+    let mut out_host = vec![0.0f32; N];
+    let mut out_host2 = vec![0.0f32; N];
 
     unsafe {
         std::ptr::copy(host_ptr_out, out_host.as_mut_ptr() as *mut c_void, Nbytes);
@@ -171,65 +178,219 @@ fn legacy() -> DeviceResult<()>{
     Ok(())
 }
 
-fn main() -> DeviceResult<()> {
-    println!("******************\ninfo: start legacy test!\n");
-    let ret = legacy();
+fn load_module<'a>(name : &str) -> DeviceResult<TopsModule>{
+    let ptx = format!("./resources/{}.o",name).to_string();
+    TopsModule::from_file(&ptx)
+}
+struct Layer<'a, T: DeviceCopy> {
+    op : &'a str,
+    weight : Option<TopsDeviceBuffer<T>>,
+    input_size : (usize, usize),
+    output_size : (usize, usize),
+    out_ref : Option<&'a TopsDeviceBuffer<T>>
+}
 
-    println!("\n\n\n******************\ninfo: start uhal tops_backend test!\n");
-
-    // Set up the context, load the module, and create a stream to run kernels in.
+fn network_test() -> DeviceResult<()> {
     let _ctx = tops::TopsApi::quick_init()?;
-
-    let ptx = "./resources/copy_d2d.o".to_string();
-    let module = TopsModule::from_file(&ptx)?;
     let stream = TopsStream::new(StreamFlags::NON_BLOCKING, None)?;
 
-    const N:usize = 100000;
-    let Nbytes : usize = N * 4;
+    const N : usize = 16;
+    const K : usize = 3;
 
-    println!("info: implicit Host2Device memory copy\n");
-    let mut src = TopsDeviceBuffer::from_slice(&[2.0f32; N])?;
-    let mut dst = TopsDeviceBuffer::from_slice(&[0.0f32; N])?;
-    let mut dst2 = TopsDeviceBuffer::from_slice(&[0.0f32; N])?;
+    //Neural network layers: matmul(tanh act) -> matmul(relu act) -> matmul(tanh act) -> convolution(3x3 kernel, tanh act) -> matmul(tanh act) -> matmul(leaky act)
+    let layers = vec![
+        Layer::<f32> {op : "matmul", weight: Some(TopsDeviceBuffer::from_slice(&[0.01f32; N * N])?), input_size : (N, N), output_size : (N, N), out_ref : None}, //weight is N x N matric for next layer
+        //Layer::<f32> {op : "tanh", weight : None, input_size : (N, N), output_size : (N, N), out_ref : None}, //out N x N
+
+        Layer::<f32> {op : "matmul", weight: Some(TopsDeviceBuffer::from_slice(&[0.02f32; N * N])?), input_size : (N, N), output_size : (N, N), out_ref : None}, //weight is N x N matric for next layer
+        //Layer::<f32> {op : "relu", weight : None, input_size : (N, N), output_size : (N, N), out_ref : None}, //out N x N
+
+        Layer::<f32> {op : "matmul", weight: Some(TopsDeviceBuffer::from_slice(&[0.02f32; N * N])?), input_size : (N, N), output_size : (N, N), out_ref : None}, //weight is N x N matric for next layer
+
+        //Layer::<f32> {op : "matmul", weight: Some(CuDeviceBuffer::from_slice(&[0.5f32; K * K])?), input_size : (N, N), output_size : (N, N), out_ref : None}, //weight is convolution kernel for next layer
+        //Layer::<f32> {op : "tanh", weight : None, input_size : (N, N), output_size : (N, N), out_ref : None}, //out N x N
+
+        //Layer::<f32> {op : "convolution", weight: Some(CuDeviceBuffer::from_slice(&[0.2f32; (N - K + 1) * (N - K + 1)])?), input_size : (N, N), output_size : (N - K + 1, N - K + 1), out_ref : None}, //weight is (N - K + 1) * (N - K + 1) matric for next layer
+       // Layer::<f32> {op : "tanh", weight : None, input_size : (N - K + 1, N - K + 1), output_size : (N - K + 1, N - K + 1), out_ref : None},  //out (N - K + 1) x (N - K + 1)
+        
+        //Layer::<f32> {op : "matmul", weight: Some(CuDeviceBuffer::from_slice(&[0.2f32; (N - K + 1) * (N - K + 1)])?), input_size : (N - K + 1, N - K + 1), output_size : (N - K + 1, N - K + 1), out_ref : None}, //weight is (N - K + 1) * (N - K + 1) matric for next layer
+        //Layer::<f32> {op : "tanh", weight : None, input_size : (N - K + 1, N - K + 1), output_size : (N - K + 1, N - K + 1), out_ref : None}, //output shape (N - K + 1) * (N - K + 1)
+
+        //Layer::<f32> {op : "matmul", weight: None, input_size : (N - K + 1, N - K + 1), output_size : (N - K + 1, N - K + 1), out_ref : None}, // no weight in the last layer
+       // Layer::<f32> {op : "leaky", weight : None, input_size : (N - K + 1, N - K + 1), output_size : (N - K + 1, N - K + 1), out_ref : None}, //output shape (N - K + 1) * (N - K + 1)
+    ];
+
+    let mut matA = TopsDeviceBuffer::from_slice(&[0.5f32; N * N])?;
+    let mut matB = TopsDeviceBuffer::from_slice(&[0.1f32; N * N])?;
+    let mut matOut = TopsDeviceBuffer::from_slice(&[0.0f32; N * N])?;
+    let mut matConvOut = TopsDeviceBuffer::from_slice(&[0.0f32; (N - K + 1) * (N - K + 1)])?;
+
+    let map_act = HashMap::from([("relu", 0), ("elu", 1), ("leaky", 2), ("tanh", 3)]);
+
+    let mut out_ref : Option<&TopsDeviceBuffer<f32>> = None;
+    for layer in layers {
+        if ["relu", "elu", "leaky", "tanh"].contains(&layer.op) {
+            let function_name = "activation_array_kernel";
+            match load_module("activation") {
+                Ok(module) => {
+                    let kernel = module.get_function(&function_name)?;
+                    unsafe {
+                        let result = launch!(kernel<<<(1, 1, 1), (layer.input_size.0 as u32, layer.input_size.1 as u32, 1), 0, stream>>>(
+                            matA.as_device_ptr(),
+                            layer.output_size.0,
+                            map_act[layer.op]
+                        ));
+                        result?;
+                    }
+                    out_ref = Some(&matA);
+                }
+                _ => { println!("Failed to load kernel!"); break;}
+            }
+        } else if layer.op == "matmul" {
+            match load_module(layer.op) {
+                Ok(module) => {
+                    let kernel = module.get_function(&layer.op)?;
+                    let mut inputShapeA = TopsDeviceBuffer::from_slice(&[layer.input_size.0 as i32, layer.input_size.1 as i32, 1i32, 1i32])?;
+                    let mut inputShapeB = TopsDeviceBuffer::from_slice(&[layer.input_size.0 as i32, layer.input_size.1 as i32, 1i32, 1i32])?;
+
+                    unsafe {
+                        let result = launch!(kernel<<<(1, 1, 1), (1, 1, 1), 0, stream>>>(
+                            matA.as_device_ptr(),
+                            matB.as_device_ptr(),
+                            matOut.as_device_ptr(),
+                            inputShapeA.as_device_ptr(),
+                            inputShapeB.as_device_ptr()
+                        ));
+                        result?;
+                    }
+                    std::mem::swap(&mut matA, &mut matOut);
+                    match layer.weight {
+                        Some(w) => { matB = w;}
+                        _ => { 
+                            // if idx < len - 1 { println!("Failed to get weight!"); break; }
+                        }
+                    }
+                    out_ref = Some(&matA);
+                }
+                _ => { println!("Failed to load kernel!"); break; }
+            }
+        } else {
+            match load_module(layer.op) {
+                Ok(module) => {
+                    let kernel = module.get_function(&layer.op)?;
+                    unsafe {
+                        let result = launch!(kernel<<<(1, 1, 1), (1, 1, 1), 0, stream>>>(
+                            matA.as_device_ptr(),
+                            matConvOut.as_device_ptr(),
+                            matB.as_device_ptr(),
+                            layer.input_size.0 as u32, layer.input_size.1 as u32,
+                            layer.output_size.0 as u32, layer.output_size.1 as u32,
+                            K,
+                            K
+                        ));
+                        result?;
+                    }
+
+                    std::mem::swap(&mut matA, &mut matConvOut);
+                    match layer.weight {
+                        Some(w) => { matB = w;}
+                        _ => { 
+                            // if idx < len - 1 { println!("Failed to get weight!"); break; }
+                        }
+                    }
+                    out_ref = Some(&matA);
+
+                }
+                _ => { println!("Failed to load kernel!"); break; }
+            }
+        }
+    }
+    // Wait asynchronous kernels to finish.
+    stream.synchronize()?;
+
+    match out_ref {
+        Some(out) => {
+            let mut conv_out_host = vec![0.0f32; out.len()];
+            out.copy_to(&mut conv_out_host[0..out.len()])?;
+            println!("\n\nResults after convolution******************");
+            for x in 0..N {
+                for y in 0..N {
+                    print!("{:.5} ", conv_out_host[x * N + y]);
+                }
+                println!("{}", "");
+            }
+        }
+        _ => { println!("Unable to obtain compute result!")}
+    }
+
+    println!("\nLaunched compute kernel successfully.");
+
+    Ok(())
+}
+
+fn main() -> DeviceResult<()> {
+    println!("******************\ninfo: start legacy test!\n");
+    legacy();
+
+    println!("******************\ninfo: start uhal tops_backend network test!\n");
+
+    network_test();
+
+    println!("\n\n\n******************\ninfo: start uhal tops_backend test!\n");
+    // Set up the context, load the module, and create a stream to run kernels in.
+    let _ctx = tops::TopsApi::quick_init()?;
+    let stream = TopsStream::new(StreamFlags::NON_BLOCKING, None)?;
+
+    // matmul of 2 x 5 and 5 x 3  -> 2 x 3
+    const M : usize = 2;
+    const N : usize = 3;
+
+    const Nbytes : usize = M * N * 4;
+    let mut matA = TopsDeviceBuffer::from_slice(&[1.0f32, 2.0f32, 3.0f32, 4.0f32, 5.0f32, 6.0f32, 1.0f32, 2.0f32, 3.0f32, 4.0f32])?;
+    let mut matB = TopsDeviceBuffer::from_slice(&[1.0f32, 2.0f32, 3.0f32, 4.0f32, 5.0f32, 1.0f32, 2.0f32, 3.0f32, 4.0f32, 5.0f32, 1.0f32, 2.0f32, 3.0f32, 4.0f32, 5.0f32])?;
+    let mut matOut = TopsDeviceBuffer::from_slice(&[0.0f32; M * N])?;
+
+    let mut out_ref : Option<&TopsDeviceBuffer<f32>> = None;
+
+    let mut inputShapeA = TopsDeviceBuffer::from_slice(&[2i32,5i32,1i32,1i32])?;
+    let mut inputShapeB = TopsDeviceBuffer::from_slice(&[5i32,3i32,1i32,1i32])?;
+
+    let mut outputShape = TopsDeviceBuffer::from_slice(&[2i32,3i32,1i32,1i32])?;
+    let mut layout = TopsDeviceBuffer::from_slice(&[1i32,0i32,2i32,3i32])?;
 
     println!("info: launching kernel!\n");
 
-    // This kernel copies elements from `src` to `dst`.
+    let fnname = "matmul";
+    let module = load_module(fnname)?;
+    let kernel = module.get_function(&fnname)?;
     unsafe {
-        // Launch the kernel with one block of one thread, no dynamic shared memory on `stream`.
-        let result = launch!(module.copy_d2d<<<1, 1, 0, stream>>>(
-            src.as_device_ptr(),
-            dst.as_device_ptr(),
-            Nbytes
-        ));
-        result?;
-
-        // Launch the kernel again using the `function` form:
-        let function_name = "copy_d2d".to_string();
-        let copy_d2d = module.get_function(&function_name)?;
-        // Launch with 1x1x1 blocks of 1x1x1 threads, to show that you can use tuples to
-        // configure grid and block size.
-        let result = launch!(copy_d2d<<<(1, 1, 1), (1, 1, 1), 0, stream>>>(
-            src.as_device_ptr(),
-            dst2.as_device_ptr(),
-            Nbytes
+        let result = launch!(kernel<<<(1, 1, 1), (1, 1, 1), 0, stream>>>(
+            matA.as_device_ptr(),
+            matB.as_device_ptr(),
+            matOut.as_device_ptr(),
+            inputShapeA.as_device_ptr(),
+            inputShapeB.as_device_ptr(),
         ));
         result?;
     }
 
     println!("info: stream synchronization...\n");
     // Kernel launches are asynchronous, so we wait for the kernels to finish executing.
-    stream.synchronize()?;   
+    stream.synchronize()?;  
 
-    println!("info: copy back to host memory!\n");
-    // Copy the results back to host memory
-    let mut out_host = [0.0f32; N*2];
-    dst.copy_to(&mut out_host[0..N])?;
-    dst2.copy_to(&mut out_host[N..2*N])?;
-
-    println!("info: cheking results...\n");
-    for x in out_host.iter() {
-        assert_eq!(2.0 as u32, *x as u32);
+    let mut host_ptr = std::ptr::null_mut();
+    let mut out_host = vec![0.0f32; M * N];
+    unsafe {
+        driv::topsHostAlloc(&mut host_ptr as *mut *mut c_void, Nbytes, 0);
+        driv::topsMemcpy(host_ptr, matOut.as_device_ptr().as_raw(), Nbytes, driv::topsMemcpyKind::topsMemcpyDeviceToHost);
+        std::ptr::copy(host_ptr, out_host.as_mut_ptr() as *mut c_void, Nbytes);
+        println!("\n\nResults of forward pass******************");
+        for x in 0..M {
+            for y in 0..N {
+                print!("{:.5} ", out_host[x * N + y]);
+            }
+            println!("{}", "");
+        }
     }
 
     println!("Launch kernel success for uhal tops!");
