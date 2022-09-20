b133403d5d02ab3b88bf5b9cdbe0f73498715fb1
guoqingbao <topon@outlook.com>
2022-09-20 13:10:08 +0800

    Support GCU activations (relu, leaky, gelu, tanh)

--- tops_backend/src/main.rs
+++ tops_backend/src/main.rs
@@ -200,15 +200,15 @@ fn network_test() -> DeviceResult<()> {
     //Neural network layers: matmul(tanh act) -> matmul(relu act) -> matmul(tanh act) -> convolution(3x3 kernel, tanh act) -> matmul(tanh act) -> matmul(leaky act)
     let layers = vec![
         Layer::<f32> {op : "matmul", weight: Some(TopsDeviceBuffer::from_slice(&[0.01f32; N * N])?), input_size : (N, N), output_size : (N, N), out_ref : None}, //weight is N x N matric for next layer
-        //Layer::<f32> {op : "tanh", weight : None, input_size : (N, N), output_size : (N, N), out_ref : None}, //out N x N
+        Layer::<f32> {op : "relu", weight : None, input_size : (N, N), output_size : (N, N), out_ref : None}, //out N x N
 
         Layer::<f32> {op : "matmul", weight: Some(TopsDeviceBuffer::from_slice(&[0.02f32; N * N])?), input_size : (N, N), output_size : (N, N), out_ref : None}, //weight is N x N matric for next layer
-        //Layer::<f32> {op : "relu", weight : None, input_size : (N, N), output_size : (N, N), out_ref : None}, //out N x N
+        Layer::<f32> {op : "tanh", weight : None, input_size : (N, N), output_size : (N, N), out_ref : None}, //out N x N
 
-        Layer::<f32> {op : "matmul", weight: Some(TopsDeviceBuffer::from_slice(&[0.02f32; N * N])?), input_size : (N, N), output_size : (N, N), out_ref : None}, //weight is N x N matric for next layer
+        // Layer::<f32> {op : "matmul", weight: Some(TopsDeviceBuffer::from_slice(&[0.02f32; N * N])?), input_size : (N, N), output_size : (N, N), out_ref : None}, //weight is N x N matric for next layer
 
-        //Layer::<f32> {op : "matmul", weight: Some(CuDeviceBuffer::from_slice(&[0.5f32; K * K])?), input_size : (N, N), output_size : (N, N), out_ref : None}, //weight is convolution kernel for next layer
-        //Layer::<f32> {op : "tanh", weight : None, input_size : (N, N), output_size : (N, N), out_ref : None}, //out N x N
+        Layer::<f32> {op : "matmul", weight: Some(TopsDeviceBuffer::from_slice(&[0.5f32; K * K])?), input_size : (N, N), output_size : (N, N), out_ref : None}, //weight is convolution kernel for next layer
+        Layer::<f32> {op : "gelu", weight : None, input_size : (N, N), output_size : (N, N), out_ref : None}, //out N x N
 
         //Layer::<f32> {op : "convolution", weight: Some(CuDeviceBuffer::from_slice(&[0.2f32; (N - K + 1) * (N - K + 1)])?), input_size : (N, N), output_size : (N - K + 1, N - K + 1), out_ref : None}, //weight is (N - K + 1) * (N - K + 1) matric for next layer
        // Layer::<f32> {op : "tanh", weight : None, input_size : (N - K + 1, N - K + 1), output_size : (N - K + 1, N - K + 1), out_ref : None},  //out (N - K + 1) x (N - K + 1)
@@ -225,20 +225,20 @@ fn network_test() -> DeviceResult<()> {
     let mut matOut = TopsDeviceBuffer::from_slice(&[0.0f32; N * N])?;
     let mut matConvOut = TopsDeviceBuffer::from_slice(&[0.0f32; (N - K + 1) * (N - K + 1)])?;
 
-    let map_act = HashMap::from([("relu", 0), ("elu", 1), ("leaky", 2), ("tanh", 3)]);
+    let map_act = HashMap::from([("relu", 0), ("gelu", 1), ("leaky", 2), ("tanh", 3)]);
 
     let mut out_ref : Option<&TopsDeviceBuffer<f32>> = None;
     for layer in layers {
-        if ["relu", "elu", "leaky", "tanh"].contains(&layer.op) {
-            let function_name = "activation_array_kernel";
-            match load_module("activation") {
+        if ["relu", "gelu", "leaky", "tanh"].contains(&layer.op) {
+            let function_name = "activation";
+            let mut inputType = TopsDeviceBuffer::from_slice(&[layer.input_size.0 as i32, layer.input_size.1 as i32, map_act[layer.op] as i32])?;
+            match load_module(function_name) {
                 Ok(module) => {
                     let kernel = module.get_function(&function_name)?;
                     unsafe {
-                        let result = launch!(kernel<<<(1, 1, 1), (layer.input_size.0 as u32, layer.input_size.1 as u32, 1), 0, stream>>>(
+                        let result = launch!(kernel<<<(1, 1, 1), (1, 1, 1), 0, stream>>>(
                             matA.as_device_ptr(),
-                            layer.output_size.0,
-                            map_act[layer.op]
+                            inputType.as_device_ptr()
                         ));
                         result?;
                     }
@@ -312,7 +312,7 @@ fn network_test() -> DeviceResult<()> {
         Some(out) => {
             let mut conv_out_host = vec![0.0f32; out.len()];
             out.copy_to(&mut conv_out_host[0..out.len()])?;
-            println!("\n\nResults after convolution******************");
+            println!("\n\nResults of forward pass******************");
             for x in 0..N {
                 for y in 0..N {
                     print!("{:.5} ", conv_out_host[x * N + y]);
@@ -330,11 +330,27 @@ fn network_test() -> DeviceResult<()> {
 
 fn main() -> DeviceResult<()> {
     println!("******************\ninfo: start legacy test!\n");
-    legacy();
+    match legacy() {
+        Ok(()) => {
+            println!("\nLaunched legacy successfully.");
+        }
+        Err(e) => {
+            println!("\nLaunche legacy failed.");
+            return Err(e);
+        }
+    }
 
     println!("******************\ninfo: start uhal tops_backend network test!\n");
 
-    network_test();
+    match network_test() {
+        Ok(()) => {
+            println!("\nLaunched network_test successfully.");
+        }
+        Err(e) => {
+            println!("\nLaunche network_test failed.");
+            return Err(e);
+        }
+    }
 
     println!("\n\n\n******************\ninfo: start uhal tops_backend test!\n");
     // Set up the context, load the module, and create a stream to run kernels in.
